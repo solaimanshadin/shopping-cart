0e40d2f8b2b369d5e77626240f69b60ca6dad136
Solaiman Shadin <solaimanshadin@gmail.com>
2020-03-01 12:02:54 +0600

    Increment/Decrement Function Optimized

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //Item 1 Increment
 const incrementItemBtn1 = document.getElementById("incrementItem1");
 incrementItemBtn1.addEventListener("click", function(){
-   itemIncrementor("itemCount1" , "itemPrice1");
+   itemCountHandler("itemCount1" , "itemPrice1" , "increment");
 });
 //Item 2 Increment
 const incrementItemBtn2 = document.getElementById("incrementItem2");
 incrementItemBtn2.addEventListener("click", function(){
-   itemIncrementor("itemCount2" , "itemPrice2");
+   itemCountHandler("itemCount2" , "itemPrice2" , "increment");
 });
 // Item 1 Decrement
 const decrementItemBtn1 = document.getElementById("decrementItem1");
 decrementItemBtn1.addEventListener("click", function(){
-   itemDecrementor( "itemCount1" , "itemPrice1");
+    itemCountHandler("itemCount1" , "itemPrice1" , "decrement");
 })
 // Item 2 Decrement
 const decrementItemBtn2 = document.getElementById("decrementItem2");
 decrementItemBtn2.addEventListener("click", function(){
-   itemDecrementor( "itemCount2" , "itemPrice2")
+    itemCountHandler("itemCount2" , "itemPrice2" , "decrement");
 })
 // Item 1 Delete
 const itemDeleteBtn1 = document.getElementById("removeItem1");
@@ -37,29 +37,11 @@ checkOutBtn.addEventListener("click", function(){
    itemsWrapper.innerHTML = '<img class="w-100" src="https://ema-john.firebaseapp.com/static/media/giphy.e800c846.gif">';
 })
 
-// Functionality for Item Increment
-function itemIncrementor(itemCountId, itemPriceId ){
+
+// Functionality for Item Increment/Decrement
+function itemCountHandler(itemCountId, itemPriceId , type){
    const itemCount = document.getElementById(itemCountId).value;
-   const newItemCount = parseFloat(itemCount) + 1;
-
-   document.getElementById(itemCountId).value = newItemCount;
-
-   const itemPrice = document.getElementById(itemPriceId).innerText;
-   const itemPriceAmount = parseFloat(itemPrice);
-
-   //Containing Original Product price Every time even after changing on dom
-   const itemOriginalPrice = itemPriceAmount / itemCount;
-
-   const incrementedPrice =  itemOriginalPrice * newItemCount;
-   document.getElementById(itemPriceId).innerText = incrementedPrice;
-   updateTotal();
-
-}
-
-// Functionality for Item Decrement
-function itemDecrementor(itemCountId, itemPriceId ){
-   const itemCount = document.getElementById(itemCountId).value;
-   const newItemCount = parseFloat(itemCount) - 1;
+   const newItemCount = type == "increment" ? parseFloat(itemCount) + 1 : parseFloat(itemCount) - 1 ;
    const itemPrice = document.getElementById(itemPriceId).innerText;
    var itemPriceAmount = parseFloat(itemPrice);
    
@@ -75,9 +57,9 @@ function itemDecrementor(itemCountId, itemPriceId ){
       document.getElementById(itemCountId).value = newItemCount;
 
       const decrementedPrice =  itemOriginalPrice * newItemCount;
-         document.getElementById(itemPriceId).innerText = decrementedPrice;
-         updateTotal();
-      }
+    document.getElementById(itemPriceId).innerText = decrementedPrice;
+    updateTotal();
+  }
 
 }
 
