d228bcbd44a639a665f312a0817310f218c95d4f
Solaiman Shadin <solaimanshadin@gmail.com>
2020-03-01 18:50:38 +0600

    favicon added and Some variable Name Changed

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 //Item 1 Increment
 const incrementItemBtn1 = document.getElementById("incrementItem1");
 incrementItemBtn1.addEventListener("click", function(){
-   itemCountHandler("itemCount1" , "itemPrice1" , "increment");
+   itemQuantityHandler("quantityOfItem1" , "priceOfItem1" , "increment");
 });
 //Item 2 Increment
 const incrementItemBtn2 = document.getElementById("incrementItem2");
 incrementItemBtn2.addEventListener("click", function(){
-   itemCountHandler("itemCount2" , "itemPrice2" , "increment");
+   itemQuantityHandler("quantityOfItem2" , "priceOfItem2" , "increment");
 });
 // Item 1 Decrement
 const decrementItemBtn1 = document.getElementById("decrementItem1");
 decrementItemBtn1.addEventListener("click", function(){
-    itemCountHandler("itemCount1" , "itemPrice1" , "decrement");
+    itemQuantityHandler("quantityOfItem1" , "priceOfItem1" , "decrement");
 })
 // Item 2 Decrement
 const decrementItemBtn2 = document.getElementById("decrementItem2");
 decrementItemBtn2.addEventListener("click", function(){
-    itemCountHandler("itemCount2" , "itemPrice2" , "decrement");
+    itemQuantityHandler("quantityOfItem2" , "priceOfItem2" , "decrement");
 })
 // Item 1 Delete
 const itemDeleteBtn1 = document.getElementById("removeItem1");
 itemDeleteBtn1.addEventListener("click", function(){
-   RemoveItemFromCart("item1", "itemPrice1")
+   RemoveItemFromCart("item1", "priceOfItem1")
 }) 
 // Item 2 Delete
 const itemDeleteBtn2 = document.getElementById("removeItem2");
 itemDeleteBtn2.addEventListener("click", function(){
-   RemoveItemFromCart("item2", "itemPrice2")
+   RemoveItemFromCart("item2", "priceOfItem2")
 })
 
 //  Item Increment/Decrement Functionality
-function itemCountHandler(itemCountId, itemPriceId , type){
-   const itemCount = document.getElementById(itemCountId).value;
-   const newItemCount = type == "increment" ? parseFloat(itemCount) + 1 : parseFloat(itemCount) - 1 ;
+function itemQuantityHandler(itemQuantityId, itemPriceId , type){
+   const itemQuantity = document.getElementById(itemQuantityId).value;
+   const newItemQuantity = type == "increment" ? parseFloat(itemQuantity) + 1 : parseFloat(itemQuantity) - 1 ;
    const itemPrice = document.getElementById(itemPriceId).innerText;
    var itemPriceAmount = parseFloat(itemPrice);
    //Containing Original Product price Every time even after changing on dom
-   const itemOriginalPrice = itemPriceAmount / itemCount;
-   // Item Count Decrement less then 1 Disabling 
-   if(newItemCount <= 1){
-      document.getElementById(itemCountId).value = 1;
+   const itemOriginalPrice = itemPriceAmount / itemQuantity;
+   // Item Quantity Decrement less then 1 Disabling 
+   if(newItemQuantity <= 1){
+      document.getElementById(itemQuantityId).value = 1;
       document.getElementById(itemPriceId).innerText = itemOriginalPrice;
    }else{
-      document.getElementById(itemCountId).value = newItemCount;
-      const updatedPrice =  itemOriginalPrice * newItemCount;
+      document.getElementById(itemQuantityId).value = newItemQuantity;
+      const updatedPrice =  itemOriginalPrice * newItemQuantity;
       document.getElementById(itemPriceId).innerText = updatedPrice;
       // DOM Updating to invoice 
-      if(itemCountId == "itemCount1" ){
+      if(itemQuantityId == "quantityOfItem1" ){
          document.getElementById("invoice-item-price1").innerText = itemOriginalPrice;
-         document.getElementById("invoice-item-quantity1").innerText = newItemCount;
+         document.getElementById("invoice-item-quantity1").innerText = newItemQuantity;
          document.getElementById("invoice-item-total1").innerText = updatedPrice;
-      }else if(itemCountId == "itemCount2" ){
+      }else if(itemQuantityId == "quantityOfItem2" ){
          document.getElementById("invoice-item-price2").innerText = itemOriginalPrice;
-         document.getElementById("invoice-item-quantity2").innerText = newItemCount;
+         document.getElementById("invoice-item-quantity2").innerText = newItemQuantity;
          document.getElementById("invoice-item-total2").innerText = updatedPrice;
       }
 
@@ -64,8 +64,8 @@ function itemCountHandler(itemCountId, itemPriceId , type){
 
 // Total/Subtotal Calculation and Update to DOM
 function updateTotal(){
-   const priceOfItem1 = document.getElementById("itemPrice1").innerText;
-   const priceOfItem2 = document.getElementById("itemPrice2").innerText;
+   const priceOfItem1 = document.getElementById("priceOfItem1").innerText;
+   const priceOfItem2 = document.getElementById("priceOfItem2").innerText;
    const getSubTotal = parseFloat(priceOfItem1) + parseFloat(priceOfItem2);
    document.getElementById('subtotal').innerText = getSubTotal;
    // Adding 5% tax with price Amount
